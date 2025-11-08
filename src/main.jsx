399e005aa22bee2d4d767344d590ceb6e00fc69b
JaySiKay <96818820+JaySiKay@users.noreply.github.com>
2025-11-08 19:20:39 +0300

    main page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '.styles/main.css'
-import '.styles/tw.css'
+import './styles/main.scss'
+import './styles/tw.css'
 // import './index.css'
 import App from './App.jsx'
 
