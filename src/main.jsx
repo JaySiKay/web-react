3c503a3ff58bda409b58916d9967df3af61e4ab3
JaySiKay <96818820+JaySiKay@users.noreply.github.com>
2025-11-28 14:54:07 +0300

    redux devtools

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import {BrowserRouter} from "react-router";
-
+import {store} from "./app/store.js";
 import App from './App.jsx'
 
 import './styles/main.scss'
@@ -11,10 +11,12 @@ import './styles/tw.css'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter>
-            <ThemeProvider initial="light">
-                <App/>
-            </ThemeProvider>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <ThemeProvider initial="light">
+                    <App/>
+                </ThemeProvider>
+            </BrowserRouter>
+        </Provider>
     </StrictMode>,
 )
